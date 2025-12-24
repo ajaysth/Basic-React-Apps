c563295bfd02b0eb7cbc81cc762ed08bb07c9130
ajaysth <shrestha11ajay@gmail.com>
2025-12-24 12:05:52 +0545

    basic useReducer App created

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react"
+
+
+type CounterState = {
+  count: number;
+};
+
+type CounterAction =
+  | { type: "INCREASE" }
+  | { type: "DECREASE" };
+
+function reducer(state: CounterState, action: CounterAction) {
+  switch (action.type) {
+    case "INCREASE":
+      return { count: state.count + 1 }
+    case "DECREASE":
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+}
+const initialState = { count: 0 }
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+        <h1 className="text-center">{state.count}</h1>
+        <button onClick={() => dispatch({ type: "INCREASE" })}>Increase</button>
+        <button onClick={() => dispatch({ type: "DECREASE" })}>Decrease</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
